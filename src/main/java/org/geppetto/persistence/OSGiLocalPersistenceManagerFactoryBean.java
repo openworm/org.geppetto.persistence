4bf99e2b09b1809f0cc422295b6c9c608f74e120
Dan D, <dan@metacell.us>
2015-01-20 14:25:08 +0200

    A version that fixes the javax.naming issue.

--- src/main/java/org/geppetto/persistence/OSGiLocalPersistenceManagerFactoryBean.java
+++ src/main/java/org/geppetto/persistence/OSGiLocalPersistenceManagerFactoryBean.java
@@ -9,7 +9,6 @@ import javax.sql.DataSource;
 import org.eclipse.gemini.blueprint.context.BundleContextAware;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
 import org.springframework.orm.jdo.LocalPersistenceManagerFactoryBean;
 
 public class OSGiLocalPersistenceManagerFactoryBean extends LocalPersistenceManagerFactoryBean implements
@@ -24,7 +23,8 @@ public class OSGiLocalPersistenceManagerFactoryBean extends LocalPersistenceMana
 	@Override
 	protected PersistenceManagerFactory newPersistenceManagerFactory(String name) {
 		return JDOHelper.getPersistenceManagerFactory(name, getClassLoader());
-//		return JDOHelper.getPersistenceManagerFactory(name, OSGiLocalPersistenceManagerFactoryBean.class.getClassLoader());
+		// return JDOHelper.getPersistenceManagerFactory(name,
+		// OSGiLocalPersistenceManagerFactoryBean.class.getClassLoader());
 	}
 
 	@Override
@@ -43,11 +43,12 @@ public class OSGiLocalPersistenceManagerFactoryBean extends LocalPersistenceMana
 		Bundle[] bundles = bundleContext.getBundles();
 
 		for (int x = 0; x < bundles.length; x++) {
-//			if ("org.datanucleus.store.rdbms".equals(bundles[x].getSymbolicName())) {
-				if ("org.datanucleus.api.jdo".equals(bundles[x].getSymbolicName())) {
+			// if ("org.datanucleus.store.rdbms".equals(bundles[x].getSymbolicName())) {
+			if ("org.datanucleus.api.jdo".equals(bundles[x].getSymbolicName())) {
 				try {
-					classloader = bundles[x].loadClass("org.datanucleus.api.jdo.JDOPersistenceManagerFactory").getClassLoader();
-//					classloader = bundles[x].loadClass("org.datanucleus.JDOClassLoaderResolver").getClassLoader();
+					classloader = bundles[x].loadClass("org.datanucleus.api.jdo.JDOPersistenceManagerFactory")
+							.getClassLoader();
+					// classloader = bundles[x].loadClass("org.datanucleus.JDOClassLoaderResolver").getClassLoader();
 				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -64,66 +65,3 @@ public class OSGiLocalPersistenceManagerFactoryBean extends LocalPersistenceMana
 		this.bundleContext = bundleContext;
 	}
 }
-
-// public class OSGiLocalPersistenceManagerFactoryBean extends
-// LocalPersistenceManagerFactoryBean implements BundleContextAware {
-//
-// public static final String JDO_BUNDLE_NAME = "org.datanucleus.api.jdo";
-// public static final String JDO_PMF_CLASS_NAME =
-// "org.datanucleus.api.jdo.JDOPersistenceManagerFactory";
-//
-// private BundleContext bundleContext;
-//
-// @Override
-// protected PersistenceManagerFactory newPersistenceManagerFactory(String name)
-// {
-// return JDOHelper.getPersistenceManagerFactory(name, getClassLoader());
-// }
-//
-// @Override
-// protected PersistenceManagerFactory newPersistenceManagerFactory(Map props) {
-// ClassLoader classLoader = getClassLoader();
-//
-// props.put("datanucleus.primaryClassLoader", classLoader);
-//
-// if (FrameworkUtil.getBundle(this.getClass()) != null) { // running in
-// // OSGi
-// props.put("datanucleus.plugin.pluginRegistryClassName",
-// "org.datanucleus.plugin.OSGiPluginRegistry");
-// }
-//
-// PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory(
-// props, classLoader);
-//
-// return pmf;
-// }
-//
-// private ClassLoader getClassLoader() {
-// ClassLoader classLoader = null;
-// Bundle thisBundle = FrameworkUtil.getBundle(this.getClass());
-//
-// if (thisBundle != null) { // on OSGi runtime
-// Bundle[] bundles = bundleContext.getBundles();
-//
-// for (Bundle bundle : bundles) {
-// if (JDO_BUNDLE_NAME.equals(bundle.getSymbolicName())) {
-// try {
-// classLoader = bundle.loadClass(JDO_PMF_CLASS_NAME)
-// .getClassLoader();
-// } catch (ClassNotFoundException e) {
-// // do something fancy here ...
-// }
-// break;
-// }
-// }
-// } else { // somewhere else
-// classLoader = this.getClass().getClassLoader();
-// }
-// return classLoader;
-// }
-//
-// @Override
-// public void setBundleContext(BundleContext bundleContext) {
-// this.bundleContext = bundleContext;
-// }
-// }
