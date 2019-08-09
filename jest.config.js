f22af66613993c94eb06748c102bd03d7758d362
jrmartin <yeshuarmartinez@gmail.com>
2019-08-08 20:24:11 -0700

    point to new persistence folder with tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = { 
   "preset": "jest-puppeteer", 
-  "testRegex": "(/__tests__/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$",
-  "testPathIgnorePatterns" : ["__tests__/TestDefaultProjects.js","__tests__/TestGeppettoModel.js","__tests__/TestLiveGeppetto.js",
-	  						  "__tests__/TestUIComponents.js"]
+  "testRegex": "(/__tests__/persistence/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$"
 };
