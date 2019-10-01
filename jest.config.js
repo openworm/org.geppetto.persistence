f5c50e0083e02804e029ee1ffc488abb5fe331d4
jrmartin <yeshuarmartinez@gmail.com>
2019-10-01 16:56:25 -0700

    tests against new folder path for tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   "preset": "jest-puppeteer", 
-  "testRegex": "(/__tests__/persistence/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$"
+  "testRegex": "(/tests/jest-puppeteer/persistence/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$"
 };
