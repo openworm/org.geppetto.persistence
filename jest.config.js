9fa72169dd04cdb2eebb3bc52aa2c4cda7a6a9ce
Afonso Pinto <afonsobspinto@gmail.com>
2019-11-28 10:51:49 +0100

    Add window global

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   "preset": "jest-puppeteer", 
-  "testRegex": "(/tests/jest-puppeteer/persistence/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$"
+  "testRegex": "(/tests/jest-puppeteer/persistence/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  "globals": {
+    "window": {}
+  }
 };
