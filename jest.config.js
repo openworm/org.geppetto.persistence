b0cd389e5a1908d3f38d6cb85a21e6884e2cac76
jrmartin <yeshuarmartinez@gmail.com>
2021-03-24 10:56:14 -0700

    #75 Replace travis with github actions

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "preset": "jest-puppeteer",
+  "testRegex": "(tests/jest-puppeteer/persistence/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  "testPathIgnorePatterns": [
+    "<rootDir>/node_modules/",
+    "<rootDir>/geppetto-client/",
+    "<rootDir>/geppetto-client/__tests__/",
+    "<rootDir>/tests/casperjs"
+  ]
+};
